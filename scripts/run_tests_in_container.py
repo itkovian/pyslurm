3f94b5d79d13df8b74313c3fc6a91d7ab72279ac
Andy Georges <itkovian@gmail.com>
2023-10-17 16:56:11 +0200

    fix: verbose building

--- scripts/run_tests_in_container.py
+++ scripts/run_tests_in_container.py
@@ -17,7 +17,7 @@ def test_run():
     host = testinfra.get_host(f"docker://slurmctl")
     python = f'python{os.environ.get("PYTHON")}'
     host.run(f'pyenv global {version_map[os.environ.get("PYTHON")]}')
-    print(host.check_output(f"{python} setup.py build"))
+    print(host.check_output(f"{python} setup.py build -v"))
     print(host.check_output(f"{python} setup.py install"))
     print(host.check_output("./scripts/configure.sh"))
     print(host.check_output(f"{python} -m pip uninstall --yes pytest"))
